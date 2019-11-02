b5694f11124d609c809c8d33b6d0a494ffc70eea
unknown <ttpnguyen2@myseneca.ca>
2019-11-01 23:49:23 -0400

    make it better

---
+++ config/key.js
@@ -0,0 +1,10 @@
+const key =
+{
+    SENDGRID_KEY: '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',
+    MONGO_URL: 'mongodb+srv://thanh-nguyen:phatnguyen@assignment-khvou.mongodb.net/assignment?retryWrites=true&w=majority'
+};
+
+
+//module.exports.MONGO_URL = key.MONGO_URL;
+//module.exports.SENDGRID_KEY= key.SENDGRID_KEY;
+module.exports.key = key;
